01bd81dd6c4119fcae134f3383d964e6d7847750
Tuấn Nguyễn <tuannguyentn2504@gmail.com>
2021-08-07 21:41:02 +0700

    ➕ Add build toggle dark mode with class style

--- dark-mode/with-class/script.js
+++ dark-mode/with-class/script.js
@@ -2,6 +2,8 @@ window.onload = function () {
 	const themeBtn = document.getElementById('toggleBtn');
 
 	themeBtn.addEventListener('click', function () {
+		document.body.classList.toggle('darkmode-active');
+
 		this.classList.toggle('active');
 	});
 };
